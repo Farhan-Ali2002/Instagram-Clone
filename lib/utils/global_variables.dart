ae397ee0a5ccee6f82d4f951860aa030c6f42a27
Farhan <farhanali0006@gmail.com>
2023-05-31 10:39:32 +0500

    global variables

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import '../screens/add_post_screen.dart';
+import '../screens/feed_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
-  Center(child: Text("feed")),
+  FeedScreen(),
   Center(child: Text("search")),
   AddPostScreen(),
   Center(child: Text("favourite")),
